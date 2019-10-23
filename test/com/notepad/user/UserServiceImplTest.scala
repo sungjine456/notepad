0fe1a8510486e0deb5feca4a792463ae0f968a86
sungjine <sungjine456@gmail.com>
2019-10-23 23:48:11 +0900

    add an initial user data for testing

--- test/com/notepad/user/UserServiceImplTest.scala
+++ test/com/notepad/user/UserServiceImplTest.scala
@@ -8,14 +8,25 @@ import com.notepad.test.DatabaseTest
 class UserServiceImplTest extends PlaySpec with BeforeAndAfter with DatabaseTest {
 
   val dao = new UserDao(provider)
+  val sequenceDao = new SequenceDao(provider)
 
-  before {
-    import dao.dbConfig.db
-    import dao.dbConfig.profile.api._
-    import dao.users
+  import dao.dbConfig.db
+  import dao.dbConfig.profile.api._
+  import dao.users
 
+  after {
     db run {
       users.delete
+    }
+  }
+
+  before {
+    db run {
+      sequenceDao.sequences.filter(_.id === "User").map(_.value).update(1)
+
+      val rows = users returning users.map(_.idx) into ((user, idx) => user.copy(idx = idx))
+
+      rows += User(1, "id", "password")
     }
   }
 
@@ -29,16 +40,17 @@ class UserServiceImplTest extends PlaySpec with BeforeAndAfter with DatabaseTest
 
   "create(id: String, password: String)" should {
     "succeed" in {
-
       val beforeFindAll: Seq[User] = await(service.findAll())
 
-      beforeFindAll.length mustBe 0
+      beforeFindAll.length mustBe 1
 
-      await(service.create("id", "pass"))
+      val user = await(service.create("id", "pass"))
 
       val afterFindAll: Seq[User] = await(service.findAll())
 
-      afterFindAll.length mustBe 1
+      afterFindAll.length mustBe 2
+      user.id mustBe "id"
+      user.password mustBe "pass"
     }
   }
 
@@ -46,7 +58,7 @@ class UserServiceImplTest extends PlaySpec with BeforeAndAfter with DatabaseTest
     "succeed" in {
       val result: Seq[User] = await(service.findAll())
 
-      result.length mustBe 0
+      result.length mustBe 1
     }
   }
 }
