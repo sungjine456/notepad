2d09b8e392272d32dbbd2bb7767caf523e3c64d6
sungjine <sungjine456@gmail.com>
2019-10-27 21:14:27 +0900

    remove example code

--- app/Module.scala
+++ app/Module.scala
@@ -1,9 +1,6 @@
 import com.google.inject.AbstractModule
-import java.time.Clock
-
 import com.notepad.post.{PostService, PostServiceImpl}
 import com.notepad.user.{UserService, UserServiceImpl}
-import services.{ApplicationTimer, AtomicCounter, Counter}
 
 /**
  * This class is a Guice module that tells Guice how to bind several
@@ -18,15 +15,7 @@ import services.{ApplicationTimer, AtomicCounter, Counter}
 class Module extends AbstractModule {
 
   override def configure() = {
-    // Use the system clock as the default implementation of Clock
-    bind(classOf[Clock]).toInstance(Clock.systemDefaultZone)
-    // Ask Guice to create an instance of ApplicationTimer when the
-    // application starts.
-    bind(classOf[ApplicationTimer]).asEagerSingleton()
     bind(classOf[UserService]).to(classOf[UserServiceImpl])
     bind(classOf[PostService]).to(classOf[PostServiceImpl])
-    // Set AtomicCounter as the implementation for Counter.
-    bind(classOf[Counter]).to(classOf[AtomicCounter])
   }
-
 }
--- client/src/app/com/notepad/main/main.component.ts
+++ client/src/app/com/notepad/main/main.component.ts
@@ -1,25 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.sass']
 })
-export class MainComponent  implements OnInit {
+export class MainComponent {
   title = 'notepad';
-  products = [];
 
-  private REST_API_SERVER = "http://localhost:9000";
-
-  constructor(private httpClient: HttpClient) { }
-
-  ngOnInit() {
-    const a = this.httpClient.get(this.REST_API_SERVER + "/count");
-
-    a.subscribe((data: any[])=>{
-      console.log(data);
-      this.products = data;
-    })
-  }
+  constructor() { }
 }
