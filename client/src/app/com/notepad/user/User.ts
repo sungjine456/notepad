58e7ae898b4ba795739f27b9638fe997b0c52205
sungjine <sungjine456@gmail.com>
2019-11-20 23:47:44 +0900

    add user registration date to the user list

--- client/src/app/com/notepad/user/User.ts
+++ client/src/app/com/notepad/user/User.ts
@@ -1,3 +1,4 @@
 export class User {
   id: String;
+  registered: Date;
 }
--- client/src/app/com/notepad/user/list/list.component.ts
+++ client/src/app/com/notepad/user/list/list.component.ts
@@ -2,23 +2,30 @@ import {Component, OnInit} from '@angular/core';
 import {environment} from "../../../../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {User} from "../User";
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.sass']
+  styleUrls: ['./list.component.sass'],
+  providers: [DatePipe]
 })
 export class ListComponent implements OnInit {
 
   list: Array<User>;
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient,
+              public datePipe: DatePipe) {
   }
 
   ngOnInit() {
     const res = this.httpClient.get(environment.server_url + "/user/list");
 
     res.subscribe((data: Array<User>) => {
+      data.forEach(value => {
+        value.registered = new Date(value.registered);
+      });
+
       this.list = data;
     });
   }
