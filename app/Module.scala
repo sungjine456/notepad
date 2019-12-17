b10d01e2baf18d3616fb66ce2da6cf8b4d26d4ab
sungjine <sungjine456@gmail.com>
2019-12-17 23:55:10 +0900

    remove unnecessary comments

--- app/Module.scala
+++ app/Module.scala
@@ -14,16 +14,6 @@ import play.api.Configuration
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationLong
 
-/**
- * This class is a Guice module that tells Guice how to bind several
- * different types. This Guice module is created when the Play
- * application starts.
- *
- * Play will automatically use any class called `Module` that is in
- * the root package. You can create modules in other locations by
- * adding `play.modules.enabled` settings to the `application.conf`
- * configuration file.
- */
 class Module(val environment: play.api.Environment,
              val config: Configuration) extends AbstractModule with ScalaModule {
 
